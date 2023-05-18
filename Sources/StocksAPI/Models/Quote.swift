4b7803a0b08d5f8c50927193a76963cbc8103952
hazed7 <iheartapplejr@gmail.com>
2023-05-18 17:34:30 +0300

    fixed quote model

--- Sources/StocksAPI/Models/Quote.swift
+++ Sources/StocksAPI/Models/Quote.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public struct QuoteResponse: Decodable {
+    
     public let data: [Quote]?
     public let error: ErrorResponse?
     
@@ -23,12 +24,11 @@ public struct QuoteResponse: Decodable {
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
         if let quoteResponseContainer = try? container.nestedContainer(keyedBy: ResponseKeys.self, forKey: .quoteResponse) {
             self.data = try? quoteResponseContainer.decodeIfPresent([Quote].self, forKey: .result)
             self.error = try? quoteResponseContainer.decodeIfPresent(ErrorResponse.self, forKey: .error)
         } else if let financeResponseContainer = try? container.nestedContainer(keyedBy: ResponseKeys.self, forKey: .finance) {
-            self.data = try? financeResponseContainer.decodeIfPresent([Quote].self, forKey: .result)
+            self.data = nil
             self.error = try? financeResponseContainer.decodeIfPresent(ErrorResponse.self, forKey: .error)
         } else {
             self.data = nil
@@ -37,21 +37,26 @@ public struct QuoteResponse: Decodable {
     }
 }
 
-public struct Quote: Codable, Identifiable, Hashable {
+public struct Quote: Decodable, Identifiable, Hashable {
+    
     public let id = UUID()
     
+    public let symbol: String
+
     public let currency: String?
     public let marketState: String?
+    
     public let fullExchangeName: String?
     public let displayName: String?
-    public let symbol: String?
+    
     public let regularMarketPrice: Double?
     public let regularMarketChange: Double?
     public let regularMarketChangePercent: Double?
     public let regularMarketChangePreviousClose: Double?
+    public let regularMarketTime: Date?
     
     public let postMarketPrice: Double?
-    public let postMarketPriceChange: Double?
+    public let postMarketChange: Double?
     
     public let regularMarketOpen: Double?
     public let regularMarketDayHigh: Double?
@@ -68,18 +73,19 @@ public struct Quote: Codable, Identifiable, Hashable {
     public let trailingAnnualDividendYield: Double?
     public let epsTrailingTwelveMonths: Double?
     
-    public init(currency: String?, marketState: String?, fullExchangeName: String?, displayName: String?, symbol: String?, regularMarketPrice: Double?, regularMarketChange: Double?, regularMarketChangePercent: Double?, regularMarketChangePreviousClose: Double?, postMarketPrice: Double?, postMarketPriceChange: Double?, regularMarketOpen: Double?, regularMarketDayHigh: Double?, regularMarketDayLow: Double?, regularMarketVolume: Double?, trailingPE: Double?, marketCap: Double?, fiftyTwoWeekLow: Double?, fiftyTwoWeekHigh: Double?, averageDailyVolume3Month: Double?, trailingAnnualDividendYield: Double?, epsTrailingTwelveMonths: Double?) {
+    public init(symbol: String, currency: String? = nil, marketState: String? = nil, fullExchangeName: String? = nil, displayName: String? = nil, regularMarketPrice: Double? = nil, regularMarketChange: Double? = nil, regularMarketChangePercent: Double? = nil, regularMarketChangePreviousClose: Double? = nil, regularMarketTime: Date? = nil, postMarketPrice: Double? = nil, postMarketChange: Double? = nil, regularMarketOpen: Double? = nil, regularMarketDayHigh: Double? = nil, regularMarketDayLow: Double? = nil, regularMarketVolume: Double? = nil, trailingPE: Double? = nil, marketCap: Double? = nil, fiftyTwoWeekLow: Double? = nil, fiftyTwoWeekHigh: Double? = nil, averageDailyVolume3Month: Double? = nil, trailingAnnualDividendYield: Double? = nil, epsTrailingTwelveMonths: Double? = nil) {
+        self.symbol = symbol
         self.currency = currency
         self.marketState = marketState
         self.fullExchangeName = fullExchangeName
         self.displayName = displayName
-        self.symbol = symbol
         self.regularMarketPrice = regularMarketPrice
         self.regularMarketChange = regularMarketChange
         self.regularMarketChangePercent = regularMarketChangePercent
         self.regularMarketChangePreviousClose = regularMarketChangePreviousClose
+        self.regularMarketTime = regularMarketTime
         self.postMarketPrice = postMarketPrice
-        self.postMarketPriceChange = postMarketPriceChange
+        self.postMarketChange = postMarketChange
         self.regularMarketOpen = regularMarketOpen
         self.regularMarketDayHigh = regularMarketDayHigh
         self.regularMarketDayLow = regularMarketDayLow
@@ -92,4 +98,5 @@ public struct Quote: Codable, Identifiable, Hashable {
         self.trailingAnnualDividendYield = trailingAnnualDividendYield
         self.epsTrailingTwelveMonths = epsTrailingTwelveMonths
     }
+    
 }
